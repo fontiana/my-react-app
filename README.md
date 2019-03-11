d33688097841f75fd47a2e75902195364f41e651
Victor Oliveira <victoroliveira@FontsLaptop.local>
2019-03-11 01:39:01 -0300

    adding css framework

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # For study purposes
 
-Nothing really special will come for you from this.
+Nothing really special will come for you from this. In fact, most of my repos are really useless for you.
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const minifyCSS = require('gulp-minify-css');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
+gulp.task('compile-less', function () {
+    gulp.src('./src/style.less')
+        .pipe(less())
+        .pipe(minifyCSS({}))
+        .pipe(concat("framework.min.css"))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch-stuff', function () {
+    gulp.watch('./src/**/*.less', ['compile-less']);
+    gulp.watch("./src/*.less").on("change", reload);
+    gulp.watch("./*.html").on("change", reload);
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['compile-less', 'watch-stuff', 'serve']);
