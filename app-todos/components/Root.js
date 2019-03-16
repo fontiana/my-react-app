d8f48bc420d6343343906b9e005d872d2a814465
Victor Oliveira <victoroliveira@FontsLaptop.local>
2019-03-16 20:37:59 -0300

    Ajuste

--- app-todos/components/Root.js
+++ app-todos/components/Root.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import { observe } from 'frint-react';
-import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { map } from 'rxjs/operator/map';
-import { merge } from 'rxjs/operator/merge';
 import { scan } from 'rxjs/operator/scan';
 
-import { addTodo } from '../actions/todos';
 import Item from './Item';
 
 class Root extends React.Component {
   render() {
     return (
       <div className="row-columns">
-
-        {/* <input
-          type="text"
-          id="todoInput"
-          value={this.props.inputValue}
-          onChange={(e) => this.props.changeInput(e.target.value)}
-        />
-
-        <button
-          type="button"
-          className="button-primary"
-          onClick={() => this.props.addTodo(this.props.inputValue, "description to be defined")}
-        >
-          Add todo
-        </button> */}
-
           {this.props.todos.map((todo, index) => (
             <Item
               key={`todo-${index}`}
@@ -50,26 +30,7 @@ export default observe(function (app) { // eslint-disable-line func-names
     };
   });
 
-  const formInput$ = new BehaviorSubject('')
-    :: map((inputValue) => {
-    return {
-      inputValue,
-    };
-  });
-  const clearInput = () => formInput$.next('');
-  const changeInput = value => formInput$.next(value);
-
-  const actions$ = Observable.of({
-    addTodo: (...args) => {
-      clearInput();
-      return store.dispatch(addTodo(...args));
-    },
-    changeInput,
-    clearInput,
-  });
-
   return state$
-    :: merge(actions$, formInput$)
     :: scan((props, emitted) => {
     return {
       ...props,
